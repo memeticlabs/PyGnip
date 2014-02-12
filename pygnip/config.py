109c009ee49fa25a92de575b19d1b03563ee60b4
tanish2k <tanish2k@gmail.com>
2014-02-13 04:32:16 +0530

    config added. setup auth using config.

---
+++ pygnip/config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""Account level configs"""
+
+
+class MainConfig(object):
+    """Main Configuration"""
+
+    # Gnip account config
+    USERNAME = ""
+    PASSWORD = ""
+    ACCOUNT_NAME = ""
+    
+    # MongoDB Details
+    MONGODB_DATABASE = 'historics'
+    MONGODB_HOST = "127.0.0.1"
+    MONGODB_PORT = 27017
+
+    # REMOTE ANALYTICS REDIS
+    REDIS_IP = '127.0.0.1'
+
+    
